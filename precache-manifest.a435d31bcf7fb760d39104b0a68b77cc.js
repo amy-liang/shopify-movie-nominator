8c9deb2d293e257e8db6cd903a5e127e6d720a62
amy-liang <amy.liaxx@gmail.com>
2020-09-03 18:45:55 -0700

    Updates

---
+++ precache-manifest.a435d31bcf7fb760d39104b0a68b77cc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ce4996a3c27863a3f1800947380f7f4",
+    "url": "/shopify-movie-nominator/index.html"
+  },
+  {
+    "revision": "298724f81dd11dc6f3d1",
+    "url": "/shopify-movie-nominator/static/css/main.5428d52f.chunk.css"
+  },
+  {
+    "revision": "51ae13c6b89628510550",
+    "url": "/shopify-movie-nominator/static/js/2.32170218.chunk.js"
+  },
+  {
+    "revision": "7a1b1c873147cb270aea317adc60a8e4",
+    "url": "/shopify-movie-nominator/static/js/2.32170218.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "298724f81dd11dc6f3d1",
+    "url": "/shopify-movie-nominator/static/js/main.b98f8bd6.chunk.js"
+  },
+  {
+    "revision": "8c792194ba888fb220aa",
+    "url": "/shopify-movie-nominator/static/js/runtime-main.8035a94c.js"
+  },
+  {
+    "revision": "dc3d4eba3d47837e758d50986ea27dfd",
+    "url": "/shopify-movie-nominator/static/media/cinema.dc3d4eba.png"
+  },
+  {
+    "revision": "070dbfaa3344e8ca2aed81a36833f554",
+    "url": "/shopify-movie-nominator/static/media/search.070dbfaa.svg"
+  },
+  {
+    "revision": "593a2a6e9c1e75618e758ae32d4ac37b",
+    "url": "/shopify-movie-nominator/static/media/trophy.593a2a6e.png"
+  }
+]);
